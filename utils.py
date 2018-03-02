49200a6e0b3c0c77d8ea866829fcb833e6fcf23d
Google Guy <Google@guy.cloud>
2018-03-02 16:32:22 +0000

    Added utilities

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from PIL import Image
+import numpy as np
 
 def preprocess_images_tf(images):
     
@@ -8,3 +10,24 @@ def preprocess_images_tf(images):
     """
     
     return ((images / 255.) - 0.5) * 2
+
+
+def take(generator, how_many):
+    
+    """
+    Take the first how_many results from a generator 
+    (or less, if the generator won't generate as many results).
+    Note that this function is also a generator.
+    """
+    
+    for _, res in zip(range(how_many), generator):
+        yield res
+        
+        
+        
+def load_image_as_np_array(image_path):
+    
+    # Pillow returns images as integers in range (0, 255)
+    
+    image = Image.open(image_path)
+    return np.array(image)
